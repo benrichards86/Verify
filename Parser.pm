6b2d49ea200f5a49bc2275f345a69c031f8d4cbc
Ben Richards <benjamin.richards86@gmail.com>
2013-06-25 13:05:26 -0400

    Fixing erroneous comments in description of instruction fields.

--- Parser.pm
+++ Parser.pm
@@ -11,15 +11,15 @@ my @instr_fields = qw/keyword modifier scope data data_action/;
 # Instruction format:
 # Keyword:       Modifier:   Scope:     Data:     Data_action:
 # ------------------------------------------------------------
-#  test          N/A         N/A        string    N/A          } Scoping instruction
-#  endtest       N/A         integer    N/A       N/A          }
-#  name          N/A         integer    string    N/A          } Declarative instruction 
-#  config        N/A         integer    string    N/A          } 
-#  description   N/A         integer    string    N/A          } 
-#  params        N/A         integer    string    +, +=        } 
-#  define        build       integer    string    +, +=        } 
-#  define        run         integer    string    +, +=        } 
-#  define        (none)      integer    string    +, +=        } 
+#  test          N/A         N/A        string    N/A          = Scoping instructions...
+#  endtest       N/A         integer    N/A       N/A          
+#  name          N/A         integer    string    N/A          = Declarative instructions...
+#  config        N/A         integer    string    N/A           
+#  description   N/A         integer    string    N/A           
+#  params        N/A         integer    string    =, +=         
+#  define        build       integer    string    =, +=         
+#  define        run         integer    string    =, +=         
+#  define        (none)      integer    string    =, +=         
 
 # Scope identifies what the current instruction is associated with.
 # State machine for parsing:
